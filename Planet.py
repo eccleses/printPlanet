51faff05c4f33feeb6fb3598002b501026f0d5ca
eccleses <107579042+eccleses@users.noreply.github.com>
2022-06-23 19:20:45 +0100

    planet code

---
+++ Planet.py
@@ -0,0 +1,22 @@
+import time
+def Number(planetNo):
+    if planetNo == 0:
+        print('The Sun')
+    if planetNo == 1:
+        print('Mercury')
+    if planetNo == 2:
+        print('Venus')
+    if planetNo == 3:
+        print('Earth')
+    if planetNo == 4:
+        print('Mars')
+    if planetNo == 5:
+        print('Jupiter')
+    if planetNo == 6:
+        print('Saturn')
+    if planetNo == 7:
+        print('Uranus')
+    if planetNo == 8:
+        print('Neptune')
+    if planetNo == 8.5:
+        print('Pluto')
